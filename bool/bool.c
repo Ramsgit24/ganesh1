defc1f4dd1f3f19677a3d099723c47522a510eed
Pavani Karamala <ramsgithub24@gmail.com>
2024-11-03 10:58:59 +0530

    adding bool files

---
+++ bool/bool.c
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	bool a=7.8f;
+	int b=true;
+	bool c='c';
+	cout<<"a="<<a<<endl;
+	cout<<"b="<<b<<endl;
+	cout<<"c="<<c<<endl;
+	return 1;
+}
+
